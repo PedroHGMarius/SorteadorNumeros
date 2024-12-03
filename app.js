b45ac397ed9ad77e5b11b2697510ba6cfa94855a
PedroHGMarius <95055081+PedroHGMarius@users.noreply.github.com>
2024-12-03 14:41:12 -0300

    1.0v

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-function obterValores() {
+function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    return { quantidade, de, ate };
+
+    let numeros_sorteados = [];
+    let numero;
+
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (numeros_sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+                
+        numeros_sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numeros_sorteados} </label>`;
+
+    alterarStatusBotao('btn-reiniciar');
+
 }
 
-function sortear() {
-    const { quantidade, de, ate } = obterValores();
-    alert(`Quantidade: ${quantidade}; Do número: ${de}; Até o número ${ate}`)
+function obterNumeroAleatorio(min,max) {
+    return Math.floor(Math. random() * (max - min + 1) + min);
+}
+
+function alterarStatusBotao(identification) {
+    let botao = document.getElementById(identification);
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao('btn-reiniciar');
 }
